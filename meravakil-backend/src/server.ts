90f2aeec69c2654676dbb45832e7991d9ff1db4d
Akash Singh <developer.akash8@gmail.com>
2025-07-10 20:25:08 +0530

    webhooK: store new user details

--- meravakil-backend/src/server.ts
+++ meravakil-backend/src/server.ts
@@ -3,8 +3,12 @@ import express from "express";
 import cors from "./middleware/cors";
 import rateLimiter from "./middleware/rateLimit";
 import chatRoute from "./api/chat";
+import storeUser from "./api/webhook";
 
 const app = express();
+
+app.post("/api/webhook", express.raw({ type: "application/json" }), storeUser);
+
 app.use(cors);
 app.use(rateLimiter);
 app.use(express.json());
