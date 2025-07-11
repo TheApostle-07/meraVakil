30fe75a7f914a0fe193608fc245493d62792f790
Akash Singh <developer.akash8@gmail.com>
2025-07-11 16:45:57 +0530

    backend: fix cors

--- meravakil-backend/src/middleware/cors.ts
+++ meravakil-backend/src/middleware/cors.ts
@@ -1,4 +1,6 @@
 // src/middleware/cors.ts
 import cors from "cors";
-export default cors({ origin: process.env.ALLOWED_ORIGIN ?? "*" });
-
+export default cors({
+  origin: process.env.ALLOWED_ORIGIN ?? "http://localhost:3000",
+  credentials: true,
+});
