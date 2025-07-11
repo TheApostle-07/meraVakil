53ac9662b798c909d1611e74a20a6d1c7712c48a
Akash Singh <developer.akash8@gmail.com>
2025-07-11 09:41:22 +0530

    schema: add isStarred to chat

--- meravakil-backend/src/api/chat/getChats.ts
+++ meravakil-backend/src/api/chat/getChats.ts
@@ -11,6 +11,7 @@ const getUserChats = async (req: Request, res: Response) => {
       select: {
         id: true,
         title: true,
+        isStarred: true,
       },
     });
     return res.status(200).json(chats);
