cb5d1288d782c4df7ac30e291a0b429847b3bf24
Akash Singh <developer.akash8@gmail.com>
2025-07-13 12:50:19 +0530

    setup in-house cors

--- meravakil-backend/src/middleware/cors.ts
+++ meravakil-backend/src/middleware/cors.ts
@@ -1,8 +1,24 @@
 // src/middleware/cors.ts
-import cors from "cors";
-export default cors({
-  origin: "https://meravakil.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-});
+import { Request, Response, NextFunction } from "express";
+
+export default function cors(req: Request, res: Response, next: NextFunction) {
+  const origin = req.headers.origin;
+  if (
+    origin === "https://meravakil.vercel.app" ||
+    origin === "http://localhost:3000"
+  ) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,OPTIONS"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  }
+  // if it’s a preflight request, just send 204
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+}
